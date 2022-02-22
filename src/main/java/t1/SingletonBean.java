97007dc65d7216b4aefe2ff2e8fa132737557e08
毛 <1296193245@qq.com>
2022-02-22 15:08:39 +0800

    Spring_Bean_scope

---
+++ src/main/java/t1/SingletonBean.java
@@ -0,0 +1,24 @@
+package t1;
+
+/**
+ * Project name(项目名称)：Spring_Bean作用域
+ * Package(包名): t1
+ * Class(类名): SingletonBean
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/2/22
+ * Time(创建时间)： 14:58
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class SingletonBean
+{
+    private String str;
+
+    public void setStr(String str)
+    {
+        this.str = str;
+    }
+}
---
+++ src/main/java/t2/PrototypeBean.java
@@ -0,0 +1,24 @@
+package t2;
+
+/**
+ * Project name(项目名称)：Spring_Bean作用域
+ * Package(包名): t2
+ * Class(类名): PrototypeBean
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/2/22
+ * Time(创建时间)： 15:04
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class PrototypeBean
+{
+    private String str;
+
+    public void setStr(String str)
+    {
+        this.str = str;
+    }
+}
